faf67b985e473cfa193021b326f9c2cfa9c00531
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-05-27 21:26:09 -0600

    3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,10 +4,10 @@ const Login = () => {
     return (
         <div>
             <h1>Hola entrenador!</h1>
-
+            <img src="/public/Ash_Ketchum_Journeys.png" alt="" />
 
             <form action="">
-               
+
                 <input type="text" name='login' placeholder='nombre de usuario' />
                 <input type="submit" />
             </form>
